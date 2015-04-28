b33f407e0853aa4496795484c28294fa3d070e82
silentmoose <silentmoose@protonmail.ch>
2015-04-28 11:26:53 -0500

    further development of the GTK GUI for the backup program. Next step is to add the actuall backup part of the script

--- driveBackup.py
+++ driveBackup.py
@@ -5,14 +5,46 @@ class MyWindow(Gtk.Window):
 
     def __init__(self):
         Gtk.Window.__init__(self, title="Drive Backup")
+	
+	self.box = Gtk.Box(spacing=5)
+	self.set_border_width(10)
+	self.add(self.box)
+	
+	listbox = Gtk.ListBox()
+	listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+	
+	row = Gtk.ListBox()
+	
+	self.box.pack_start(listbox, True,True,0)
+	self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        
+	row.add(self.box)
+	
+	
+	vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+	self.box.pack_start(vbox, True, True, 0)
+	
+	homeButton = Gtk.CheckButton("Backup /home")
+	vbox.pack_start(homeButton,True, True, 1)
+	
+	systemButton = Gtk.CheckButton("Backup /")
+	vbox.pack_start(systemButton,True,True,2)
+		
 	self.start_button = Gtk.Button(label="Backup")
         self.start_button.connect("clicked", self.on_button_clicked)
-        self.add(self.start_button)
+        
+	vbox.pack_start(self.start_button,True, True, 1)
+	
+	listbox.add(row)
+
 	
     def on_button_clicked(self, widget):
       self.start_backup() 
     def start_backup(self):
        print("starting backup")
+
+#init shit
+
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
