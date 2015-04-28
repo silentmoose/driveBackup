a9e10288bb184d957adc8365603b06707e0338b0
silentmoose <silentmoose@protonmail.ch>
2015-04-28 11:37:23 -0500

    Switched to Switches instead of checkboxes for design Will add labels and another horizontalbox next commit

--- driveBackup.py
+++ driveBackup.py
@@ -6,6 +6,11 @@ class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Drive Backup")
 	
+	hb = Gtk.HeaderBar()
+	hb.set_show_close_button(True)
+	hb.props.title = "Drive Backup"
+	self.set_titlebar(hb)
+	
 	self.box = Gtk.Box(spacing=5)
 	self.set_border_width(10)
 	self.add(self.box)
@@ -24,11 +29,11 @@ class MyWindow(Gtk.Window):
 	vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 	self.box.pack_start(vbox, True, True, 0)
 	
-	homeButton = Gtk.CheckButton("Backup /home")
-	vbox.pack_start(homeButton,True, True, 1)
+	homeSwitch= Gtk.Switch()
+	vbox.pack_start(homeSwitch,True, True, 1)
 	
-	systemButton = Gtk.CheckButton("Backup /")
-	vbox.pack_start(systemButton,True,True,2)
+	systemSwitch= Gtk.Switch()
+	vbox.pack_start(systemSwitch,True,True,2)
 		
 	self.start_button = Gtk.Button(label="Backup")
         self.start_button.connect("clicked", self.on_button_clicked)
